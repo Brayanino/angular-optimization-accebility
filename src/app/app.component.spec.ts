887b1452250737678b3d0eb13f81801841eb39db
bnino <bnino@bancolombia.com.co>
2025-09-24 11:07:12 -0500

    Fork init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-sofkau'`, () => {
+  it(`should have as title 'angular-sofkau '`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-sofkau');
